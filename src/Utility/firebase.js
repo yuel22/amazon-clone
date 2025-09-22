22a36e42db5b968572bd1faa5ebcc93b5cf462fd
yuel22 <yueladhanom@gmail.com>
2025-09-22 15:48:21 +0300

    setup firebase

---
+++ src/Utility/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+// auth
+import { getAuth } from "firebase/auth";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBbw-MTZQGWKWPEX2BGz7EBk2A_QZQM1M0",
+  authDomain: "clone-674d6.firebaseapp.com",
+  projectId: "clone-674d6",
+  storageBucket: "clone-674d6.firebasestorage.app",
+  messagingSenderId: "102361656144",
+  appId: "1:102361656144:web:6911ce22f2fc52a31b5a55",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = app.firestore();
