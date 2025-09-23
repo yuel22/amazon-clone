bee0539280590304c22754d17ce2b63bc0cbd836
yuel22 <yueladhanom@gmail.com>
2025-09-24 00:20:06 +0300

    Set up backend payment API using Firebase function

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,45 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
+const { onRequest } = require("firebase-functions/https");
 const logger = require("firebase-functions/logger");
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const { setGlobalOptions } = require("firebase-functions");
+dotenv.config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const app = express();
+app.use(cors({ origin: true }));
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Success !",
+  });
+});
+
+
+app.post("/payment/create", async (req, res) => {
+  const total = parseInt(req.query.total);
+  console.log(req);
+
+  if (total > 0) {
+    // console.log("Payment received:", total);
+    // res.send(`Payment received: $${total}`);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: total,
+      currency: "usd",
+    });
+    res.status(201).json({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } else {
+    res.status(403).json({
+      message: "You have to select an item to paid.",
+    });
+  }
+});
+
+exports.api = onRequest(app);
