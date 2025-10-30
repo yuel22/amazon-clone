8c1df53e4a0c17f80f573ddbd858ea730ad97705
yuel22 <yueladhanom@gmail.com>
2025-10-29 19:14:17 -0700

    fix: revert react version to 19.1.1 and adjust import statement in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import DataProvider from "./Components/DataProvider/DataProvider.jsx";
 import { initialState, reducer } from "./Utility/reducer.js";
-
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
