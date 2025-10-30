7d3a7bf3b2be1e616eb4754eb18b7d010c6843a6
yuel22 <yueladhanom@gmail.com>
2025-10-29 20:03:07 -0700

    Fix Layout import path to match actual file casing

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,6 +1,6 @@
 import CarouselBanner from "../../Components/Carousel/Carousel";
 import Category from "../../Components/Category/Category";
-import Layout from "../../Components/Layout/Layout";
+import Layout from "../../Components/LayOut/LayOut";
 import Product from "../../Components/Product/Product";
 
 const Landing = () => {
